8317d98612506e46b8582a083030bf6cf6da3262
Marharyta Zazulia <zazmargo@gmail.com>
2024-11-23 22:24:03 -0300

    update settings

--- lets_go_tango/settings.py
+++ lets_go_tango/settings.py
@@ -28,10 +28,13 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
